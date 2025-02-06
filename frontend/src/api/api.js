41f7c41edce844690b61287e201001963fe46872
JoseManuel <josemak87@hotmail.es>
2025-02-06 14:21:27 +0100

    Arreglo

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_BACKEND_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000", // Usa import.meta.env para compatibilidad con Vite
 });
 
 export default api;
-
