bf659be37b9dd6eb7a9f6ce12203302ed6e9067c
JoseManuel <josemak87@hotmail.es>
2025-02-06 14:15:04 +0100

    Cambios en api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || "https://bazar-josy-production.up.railway.app",
+  baseURL: process.env.VITE_BACKEND_URL || "http://localhost:3000",
 });
 
 export default api;
+
