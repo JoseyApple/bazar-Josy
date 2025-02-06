b0b9a8a9b9f005042c988967538a1834575aa072
JoseManuel <josemak87@hotmail.es>
2025-02-06 14:28:35 +0100

    Arreglos

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,5 @@
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://bazar-josy-production.up.railway.app",
+  baseURL: "https://bazar-josy-production.up.railway.app",
 });
 
-console.log("Base URL:", import.meta.env.VITE_BACKEND_URL);
 export default api;
-
